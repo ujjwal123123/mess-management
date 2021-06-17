20c73c1963773126bb9fd50418f8b6d7c94b224e
Anshuman <anshuman8800@gmail.com>
2021-06-18 00:17:40 +0530

    Remove unnesseary variable's name

--- routes/settings.js
+++ routes/settings.js
@@ -13,22 +13,22 @@ router.get("/", function (req, res) {
 
 router.post("/update_password", function (req, res) {
   if (req.session.userId) {
-    let cPass = req.body.current_password;
-    let nPass = req.body.new_password; //$2b$10$BWRGottmRk7cPE7BawRPi.RxbdHBbJJEqU08DKkXo.YOZdxjg7ZsC
-    let uName = req.session.userId; // console.log("cpass"+cPass+" npass"+nPass+" uname"+uName);
     database.getConnection().then((conn) => {
       // TODO: handle expected errors
       conn
-        .query("SELECT * FROM Users WHERE login_id = ?", [uName])
+        .query("SELECT * FROM Users WHERE login_id = ?", [req.session.userId])
         .then(async (rows) => {
-          if (await bcrypt.compare(cPass, rows[0].login_pass)) {
+          if (
+            await bcrypt.compare(req.body.current_password, rows[0].login_pass)
+          ) {
             console.log("password match perfectly");
             conn.end();
+            const hashPassword = await bcrypt.hash(req.body.new_password, 10);
             database.getConnection().then((conn) => {
               conn //hash the password
                 .query("UPDATE Users SET login_pass = ? WHERE (login_id = ?)", [
-                  nPass,
-                  uName,
+                  hashPassword,
+                  req.session.userId,
                 ]);
               conn.end();
             });
