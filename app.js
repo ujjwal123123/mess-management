07455f47f6068e9a8bee4ea44b3eb754ebb82073
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-06-19 20:14:46 +0530

    Improve error handling

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.status = err.status;
+  res.locals.stack =
+    req.app.get("env") === "development" ? err.stack : undefined;
 
   // render the error page
   res.status(err.status || 500);
