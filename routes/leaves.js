f7e00a566563c75bd2dfa1eebc0e4e89199c9f4c
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-06-22 18:49:25 +0530

    Refactor code

--- routes/leaves.js
+++ routes/leaves.js
@@ -2,47 +2,36 @@ const express = require("express");
 const router = express.Router();
 const database = require("../database");
 
-router.post("/", function (req, res, next) {
+router.post("/", async function (req, res, next) {
   if (Date.parse(req.body.start_date) > Date.parse(req.body.end_date)) {
     next(Error("INVALID FORMAT: Select end date after start date"));
     return;
   }
-  database.getConnection().then((conn) => {
-    conn
-      .query("SELECT * FROM Students WHERE roll_no = ?", [
-        parseInt(req.body.roll_no),
-      ])
-      .then((rows) => {
-        if (rows[0]) {
-          conn.end();
-          database.getConnection().then((conn) => {
-            // TODO: validate data before insertion
-            conn
-              .query("INSERT INTO Leaves VALUES (?,?,?,?);", [
-                parseInt(req.body.roll_no),
-                req.body.start_date,
-                req.body.end_date,
-                req.body.remark,
-              ])
-              .then((data) => {
-                conn.end();
-              })
-              .catch((err) => {
-                next(err);
-                conn.end();
-              });
-          });
-        } else {
-          next(Error("The roll no could not be found in database"));
-          conn.end();
-          return;
-        }
-      })
-      .catch((err) => {
-        next(err);
-        conn.end();
-      });
-  });
+  const roll_no = parseInt(req.body.roll_no);
+
+  let conn;
+  try {
+    conn = await database.getConnection();
+
+    const rows = await conn.query("SELECT * FROM Students WHERE roll_no = ?", [
+      roll_no,
+    ]);
+
+    if (rows.length !== 1) {
+      throw Error("The roll no could not be found in database");
+    }
+
+    await conn.query("INSERT INTO Leaves VALUES (?,?,?,?)", [
+      roll_no,
+      req.body.start_date,
+      req.body.end_date,
+      req.body.remark,
+    ]);
+  } catch (err) {
+    next(err);
+  } finally {
+    if (conn) await conn.end();
+  }
 });
 
 module.exports = router;
