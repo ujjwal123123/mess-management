393aecb21a9e67b1be8df95ecdf218f47157d269
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-06-26 01:00:03 +0530

    Use knex

--- routes/rate.js
+++ routes/rate.js
@@ -3,35 +3,24 @@ const database = require("../database");
 const router = express.Router();
 
 router.get("/", async function (req, res, next) {
-  let conn;
-
   try {
-    conn = await database.getConnection();
-    const sql =
-      "select DATE_FORMAT(start_date, '%d %M %Y') as start_date, rate from Rate";
-    const rates = await conn.query(sql);
+    const rates = await database("Rate").select();
     res.render("rate", { items: rates });
   } catch (err) {
     next(err);
-  } finally {
-    if (conn) await conn.end();
   }
 });
 
 router.post("/", async function (req, res, next) {
-  let conn;
-
   // TODO: validate data before insertion
   try {
-    conn = await database.getConnection();
-    await conn.query("INSERT INTO Rate (start_date, rate) VALUES (?,?)", [
-      req.body.start_date,
-      req.body.rate,
-    ]);
+    await database("Rate").insert({
+      start_date: req.body.start_date,
+      rate: req.body.rate,
+    });
+    res.redirect("/rate")
   } catch (err) {
     next(err);
-  } finally {
-    if (conn) await conn.end();
   }
 });
 
