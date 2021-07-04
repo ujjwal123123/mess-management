9743059d7ab50bab9d93b420e9baedd59faf0225
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-07-05 04:21:40 +0530

    Validate semester data before insertion

--- routes/semester.js
+++ routes/semester.js
@@ -12,8 +12,39 @@ router.get("/", async function (req, res, next) {
 });
 
 router.post("/", async function (req, res, next) {
-  // TODO: validate data before insertion
   try {
+    // validate data before insertion
+    // req.body.start_date must not lie inside a semester
+    const start_date_array = await database("Semesters").whereRaw(
+      "start_date < ? AND end_date > ? AND program = ? AND year_of_admission = ?",
+      [
+        req.body.end_date,
+        req.body.end_date,
+        req.body.program,
+        req.body.year_of_admission,
+      ]
+    );
+    if (start_date_array.length > 0) {
+      throw Error("Invalid start date entered");
+    }
+    // req.body.end_date must not lie inside a semester
+    const end_date_array = await database("Semesters").whereRaw(
+      "start_date < ? AND end_date > ? AND program = ? AND year_of_admission = ?",
+      [
+        req.body.end_date,
+        req.body.end_date,
+        req.body.program,
+        req.body.year_of_admission,
+      ]
+    );
+    if (end_date_array.length > 0) {
+      throw Error("Invalid end date entered");
+    }
+
+    if (Date.parse(req.body.end_date) <= Date.parse(req.body.start_date)) {
+      throw Error("End date must be after start date");
+    }
+
     await database("Semesters").insert({
       start_date: req.body.start_date,
       end_date: req.body.end_date,
