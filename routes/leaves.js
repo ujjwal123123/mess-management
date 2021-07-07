e2fdb25a3fb0bd940d7a1d4626038aba48ebff72
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-07-08 03:42:15 +0530

    More validation work

--- routes/leaves.js
+++ routes/leaves.js
@@ -2,14 +2,41 @@ const express = require("express");
 const router = express.Router();
 const database = require("../database");
 
-router.post("/", async function (req, res, next) {
-  if (Date.parse(req.body.start_date) > Date.parse(req.body.end_date)) {
-    next(Error("INVALID FORMAT: Select end date after start date"));
-    return;
-  }
-  const roll_no = parseInt(req.body.roll_no);
+/**
+ *
+ * @param {Date} date
+ * @param {number} roll_no
+ * @returns {Promise<boolean>}
+ */
+async function dateInLeaves(date, roll_no) {
+  const rows = await database("Leaves")
+    .where("start_date", "<=", date)
+    .where("end_date", ">", date)
+    .where("roll_no", roll_no);
 
+  return rows.length > 0;
+}
+
+router.post("/", async function (req, res, next) {
   try {
+    const roll_no = parseInt(req.body.roll_no);
+
+    // validate data before insertion
+    // req.body.start_date must not lie insdie a leave
+
+    if (await dateInLeaves(req.body.start_date, roll_no)) {
+      throw Error("Invalid start date entered");
+    }
+
+    // req.body.end_date must not lie inside a leave
+    if (await dateInLeaves(req.body.end_date, roll_no)) {
+      throw Error("Invalid end date entered");
+    }
+
+    if (Date.parse(req.body.start_date) > Date.parse(req.body.end_date)) {
+      throw Error("INVALID FORMAT: Select end date after start date");
+    }
+
     const students = await database("Students")
       .select()
       .where("roll_no", roll_no);
--- routes/semester.js
+++ routes/semester.js
@@ -11,27 +11,45 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+/**
+ *
+ * @param {Date} date
+ * @param {string} program
+ * @param {number} year_of_admission
+ * @returns {Promise<boolean>}
+ */
+async function dateInSemester(date, program, year_of_admission) {
+  const rows = await database("Semesters")
+    .where("start_date", "<=", date)
+    .where("end_date", ">", date)
+    .where("program", program)
+    .where("year_of_admission", year_of_admission);
+
+  return rows.length > 0;
+}
+
 router.post("/", async function (req, res, next) {
   try {
     // validate data before insertion
     // req.body.start_date must not lie inside a semester
-    const start_date_array = await database("Semesters")
-      .where("start_date", "<", req.body.end_date)
-      .where("end_date", ">", req.body.end_date)
-      .where("program", req.body.program)
-      .where("year_of_admission", req.body.year_of_admission);
-
-    if (start_date_array.length > 0) {
+    if (
+      await dateInSemester(
+        req.body.start_date,
+        req.body.program,
+        req.body.year_of_admission
+      )
+    ) {
       throw Error("Invalid start date entered");
     }
-    // req.body.end_date must not lie inside a semester
-    const end_date_array = await database("Semesters")
-      .where("start_date", "<", req.body.end_date)
-      .where("end_date", ">", req.body.end_date)
-      .where("program", req.body.program)
-      .where("year_of_admission", req.body.year_of_admission)
 
-    if (end_date_array.length > 0) {
+    // req.body.end_date must not lie inside a semester
+    if (
+      await dateInSemester(
+        req.body.end_date,
+        req.body.program,
+        req.body.year_of_admission
+      )
+    ) {
       throw Error("Invalid end date entered");
     }
 
