6f54096bd4e86a505cc53290bec36c553c60b867
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-07-13 00:53:00 +0530

    Filtering workaround

--- public/js/tabulator/student.js
+++ public/js/tabulator/student.js
@@ -1,14 +1,16 @@
 const table = new Tabulator("#students-table", {
   autoColumns: false, // create columns from data field names
   columns: [
-    { title: "Name", field: "name" },
-    { title: "Email", field: "email" },
-    { title: "Roll no", field: "roll_no" },
-    { title: "Hostel", field: "hostel_name" },
+    { title: "Name", field: "name", headerFilter: true },
+    { title: "Email", field: "email", headerFilter: true },
+    { title: "Roll no", field: "roll_no", headerFilter: true },
+    { title: "Hostel", field: "hostel_name", headerFilter: true },
   ],
+  layout: "fitDataTable",
 });
 table.setData(window.location.href + "/json");
 
+// download button
 const downloadButton = document.getElementById("download_button");
 downloadButton.addEventListener("click", () => {
   table.download("csv", "data.csv");
