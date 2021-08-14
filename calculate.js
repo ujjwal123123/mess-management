ac7f6951d55e0772d98cea1463ebefbe90112bc0
Anshuman <anshuman8800@gmail.com>
2021-08-14 13:42:23 +0530

    Bug fixed and modify date objects

--- calculate.js
+++ calculate.js
@@ -69,12 +69,24 @@ async function calculateAmount(start_date, end_date, roll_no) {
   utils.assert(start_date < end_date);
 
   let amount = 0;
-  let daysPresent = 0; // number of days the student was present in the hostel
+  let daysPresent = 0;
 
   let itr_date = start_date;
+
   while (itr_date <= end_date) {
-    if (await isPresentOnDate(itr_date, roll_no)) {
-      amount += await rateOnDate(itr_date);
+    //MUST HAVE TO CONVERT ITR-DATE TO STRING
+    var loop_date = itr_date.toLocaleString(undefined, {
+      timeZone: "Asia/Kolkata",
+    });
+    loop_date =
+      loop_date.substring(6, 10) +
+      "-" +
+      loop_date.substring(3, 5) +
+      "-" +
+      loop_date.substring(0, 2);
+
+    if (await isPresentOnDate(loop_date, roll_no)) {
+      amount += await rateOnDate(loop_date);
       daysPresent++;
     }
     itr_date.setDate(itr_date.getDate() + 1);
@@ -98,12 +110,23 @@ async function getAmountList(roll_no) {
     .where("year_of_admission", yearOfAdmission)
     .where("program", program);
 
-  let startDate = new Date(semesters[0].start_date);
-  let currentDate = new Date();//TODO
-  let closingDate = new Date("2021-01-01");
-  if(currentDate-closingDate>0)
-    currentDate = closingDate;
+  let miniDate = semesters[0].start_date; //select the very 1st semester date of student
+  for (let i = 0; i < semesters.length; i++) {
+    if (semesters[i].start_date < miniDate) {
+      miniDate = semesters[i].start_date;
+    }
+  }
 
+  let startDate = new Date(miniDate);
+  startDate.setMonth(startDate.getMonth() - 1); //some adjustment [don't remove IMP]
+  let currentDate = new Date();
+  let closingD = await database("Students")
+    .select("closing_date")
+    .where("roll_no", roll_no);
+  let closingDate = new Date(closingD[0].closing_date);
+
+  if (currentDate - closingDate > 0) currentDate = closingDate;
+  console.log(currentDate + "----" + startDate);
   while (currentDate >= startDate) {
     const Start =
       currentDate.getFullYear().toString() +
@@ -118,22 +141,22 @@ async function getAmountList(roll_no) {
       new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)
         .getDate()
         .toString();
-
     const row = await calculateAmount(new Date(Start), new Date(End), roll_no);
-    // var month_name = 
+    var month_name = currentDate.toDateString();
+    month_name = month_name.substring(4, 8) + month_name.substring(11, 15);
     amountList.push({
-      month: Start,
+      month: month_name,
       daysPresent: row[1],
       amount: row[0],
     });
 
     currentDate.setMonth(currentDate.getMonth() - 1);
   }
-  // console.log(closing_date);
   return amountList;
 }
 
-getAmountList(1703100).then((data) => {
+getAmountList(2001098).then((data) => {
+  //here put roll_no to check
   console.log(data);
   console.log("successfully");
 });
